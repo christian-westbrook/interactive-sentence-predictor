1214448d87687046ecfb3208e433969684a018a3
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2018-10-01 17:55:04 -0500

    Create Node.java

---
+++ runtime/predictor/Node.java
@@ -0,0 +1,33 @@
+package predictor;
+
+public class Node {
+	
+	private String val;
+	private double prob;
+	
+	public Node(String val, double prob) {
+		
+		this.val = val;
+		this.prob = prob;
+	}
+	
+	public void setValues(String val, double prob) {
+		
+		this.val = val;
+		this.prob = prob;
+		
+	}
+
+	public String getVal() {
+		
+		return val;
+		
+	}
+	
+	public double getProb() {
+		
+		return prob;
+		
+	}
+
+}
