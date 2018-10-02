531e45db04a83e01b7c4104e5560a15e130964ec
Christian Westbrook <christianwestbrook@live.com>
2018-10-01 19:37:08 -0500

    Testing a shell script that will run the preprocessor

--- preprocessor/run.sh
+++ preprocessor/run.sh
@@ -1,1 +1,9 @@
-bash ./tokenizer/tokenizer.sh ./training-data/ ./tokens/
+#!/bin/sh
+
+# Configures this shell script to exit immediately if an error occurs.
+# This includes any program returning an exit code other than 0
+set -e
+
+# Move into the tokenizer directory and compile the JavaCC tokenizer
+cd ./tokenizer
+javacc PS2.jj
