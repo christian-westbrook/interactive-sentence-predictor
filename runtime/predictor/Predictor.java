26bfff9cac1cf0a14e2914a006a12745cc27b482
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2018-10-02 16:51:52 -0500

    Update Predictor.java

--- runtime/predictor/Predictor.java
+++ runtime/predictor/Predictor.java
@@ -1,4 +1,4 @@
-package predictor;
+
 
 import java.util.HashMap;
 import java.util.Iterator;
@@ -6,245 +6,257 @@ import java.util.Map;
 
 public class Predictor {
 
-	HashMap<String, Integer> unigrams;
-	HashMap<String, Integer> bigrams;
-	HashMap<String, Integer> trigrams;
-	HashMap<String, Integer> misc;
+    HashMap<String, Integer> unigrams;
+    HashMap<String, Integer> bigrams;
+    HashMap<String, Integer> trigrams;
+    HashMap<String, Integer> misc;
 
-	public Predictor(HashMap<String, Integer> unigrams, HashMap<String, Integer> bigrams,
-			HashMap<String, Integer> trigrams, HashMap<String, Integer> misc) {
+    public Predictor(HashMap<String, Integer> unigrams, HashMap<String, Integer> bigrams,
+            HashMap<String, Integer> trigrams) {
 
-		this.unigrams = unigrams;
-		this.bigrams = bigrams;
-		this.trigrams = trigrams;
-		this.misc = misc;
+        this.unigrams = unigrams;
+        this.bigrams = bigrams;
+        this.trigrams = trigrams;
+        
+        misc = new HashMap<>();
+        
+        calcMisc(unigrams,"UNI_N","UNI_V");
+        //calcMisc(bigrams,"BI_N","BI_V");
+        //calcMisc(trigrams,"TRI_N","TRI_V");
 
-	}
+    }
 
-	// ===========================================================================================================
-	// Accepts input from Voce, a single string.
-	//
-	// ===========================================================================================================
+    public void calcMisc(HashMap<String, Integer> hm, String key1, String key2) {
 
-	public String bigramSentence(String line) {
+        Iterator i = hm.entrySet().iterator();
+        int n = 0;
+        int v = 0;
 
-		StringBuilder sentence = new StringBuilder();
-		String[] spl;
-		String first;
+        while (i.hasNext()) {
 
-		spl = line.split(" ");
-		first = spl[spl.length - 1];
+            n += (int)((Map.Entry)i.next()).getValue();
+            v++;
 
-		buildBigramResult(sentence, first, 1.0, 10);
+        }
 
-		return sentence.toString();
+        misc.put(key1,n);
+        misc.put(key2,v);
 
-	}
+    }
 
-	public void buildBigramResult(StringBuilder sb, String prev, double prob, int count) {
+    // ===========================================================================================================
+    // Accepts input from Voce, a single string.
+    //
+    // ===========================================================================================================
+    public String bigramSentence(String line) {
 
-		Node n;
+        StringBuilder sentence = new StringBuilder();
+        String[] spl;
+        String first;
+        
+        spl = line.split(" ");
+        first = spl[spl.length - 1];
 
-		if (count > 0) {
+        buildBigramResult(sentence, first, 1.0, 10);
 
-			n = bigramMLE(prev);
+        return sentence.toString();
 
-			sb.append(n.getVal() + " ");
+    }
 
-			prob = prob * n.getProb();
+    public void buildBigramResult(StringBuilder sb, String prev, double prob, int count) {
 
-			buildBigramResult(sb, n.getVal(), prob, --count);
+        Node n;
 
-		} else {
+        if (count > 0) {
 
-			sb.append(prob);
+            n = bigramMLE(prev);
 
-		}
+            sb.append(n.getVal() + " ");
 
-	}
+            prob = prob * n.getProb();
 
-	// ===========================================================================================================
-	// Accepts input from Voce, a single string.
-	//
-	// ===========================================================================================================
+            buildBigramResult(sb, n.getVal(), prob, --count);
 
-	public String trigramSentence(String line) {
+        } else {
 
-		StringBuilder sentence = new StringBuilder();
-		String[] spl;
-		String first;
-		String second;
+            sb.append(prob);
 
-		String prev;
-		String next;
+        }
 
-		spl = line.split(" ");
+    }
 
-		first = spl[spl.length - 2]; // Could be an issue of the line of the spoken line is less than three.
-		second = spl[spl.length - 1];
+    // ===========================================================================================================
+    // Accepts input from Voce, a single string.
+    //
+    // ===========================================================================================================
+    public String trigramSentence(String line) {
 
-		buildTrigramResult(sentence, first, second, 1.0, 10);
+        StringBuilder sentence = new StringBuilder();
+        String[] spl;
+        String first;
+        String second;
 
-		return sentence.toString();
+        spl = line.split(" ");
 
-	}
+        first = spl[spl.length - 2]; // Could be an issue of the line of the spoken line is less than three.
+        second = spl[spl.length - 1];
 
-	public void buildTrigramResult(StringBuilder sb, String first, String second, double prob, int count) {
+        buildTrigramResult(sentence, first, second, 1.0, 10);
 
-		Node n;
+        return sentence.toString();
 
-		if (count > 0) {
+    }
 
-			n = trigramMLE(first, second);
+    public void buildTrigramResult(StringBuilder sb, String first, String second, double prob, int count) {
 
-			if (n.getProb() == Double.NEGATIVE_INFINITY) {
+        Node n;
 
-				n = bigramMLE(second);
+        if (count > 0) {
 
-			}
+            n = trigramMLE(first, second);
 
-			sb.append(n.getVal() + " ");
+            if (n.getProb() == Double.NEGATIVE_INFINITY) {
 
-			prob = prob * n.getProb();
+                n = bigramMLE(second);
 
-			buildTrigramResult(sb, second, n.getVal(), prob, --count);
+            }
 
-		} else {
+            sb.append(n.getVal() + " ");
 
-			sb.append(prob);
+            prob = prob * n.getProb();
 
-		}
+            buildTrigramResult(sb, second, n.getVal(), prob, --count);
 
-	}
+        } else {
 
-	// ===========================================================================================================
-	// Calculate unigram MLE. I'm not sure if we actually need this.
-	//
-	// ===========================================================================================================
+            sb.append(prob);
 
-	public Node unigramMLE() {
+        }
 
-		Node res = new Node("", Double.NEGATIVE_INFINITY); // Technically a zero probability. Either this or return
-															// null, & check for null.
-		String s;
-		double min = Double.NEGATIVE_INFINITY;
-		double log;
+    }
 
-		Iterator i = unigrams.entrySet().iterator();
+    // ===========================================================================================================
+    // Calculate unigram MLE. I'm not sure if we actually need this.
+    //
+    // ===========================================================================================================
+    public Node unigramMLE() {
 
-		while (i.hasNext()) {
+        Node res = new Node("", Double.NEGATIVE_INFINITY); // Technically a zero probability. Either this or return
+        // null, & check for null.
+        String s;
+        double min = Double.NEGATIVE_INFINITY;
+        double log;
 
-			s = ((Map.Entry) i.next()).getKey().toString();
+        Iterator i = unigrams.entrySet().iterator();
 
-			/*
+        while (i.hasNext()) {
+
+            s = ((Map.Entry) i.next()).getKey().toString();
+
+            /*
 			 * LOG PROBABILITY NOTES: Probabilities occur between zero and one. The smallest
 			 * probability is 0 and the greatest is 1. In Java, the range of a log
 			 * probability is -infinity to 0. Fractions return negative value from a log
 			 * function. So, we're looking for negative exponents closest to zero.
-			 */
+             */
+            // .size() isn't an adequate solution. We need N in this case.
+            log = Math.log10((double) unigrams.get(s) / misc.get("UNI_N"));
 
-			// .size() isn't an adequate solution. We need N in this case.
+            if (log > min) {
 
-			log = Math.log10((double) unigrams.get(s) / unigrams.size());
+                min = log;
+                res.setValues(s, min);
 
-			if (log > min) {
+            }
 
-				min = log;
-				res.setValues(s, min);
+        }
 
-			}
+        return res;
 
-		}
+    }
 
-		return res;
+    // ===========================================================================================================
+    // Calculate bigram MLE from a single word input. This is called after the input
+    // from Voce is split
+    // into fragments we can use.
+    // ===========================================================================================================
+    public Node bigramMLE(String first) {
 
-	}
+        Node res = new Node("", Double.NEGATIVE_INFINITY);
+        String s;
+        String e;
+        double min = Double.NEGATIVE_INFINITY;
+        double log;
 
-	// ===========================================================================================================
-	// Calculate bigram MLE from a single word input. This is called after the input from Voce is split
-	// into fragments we can use.
-	// ===========================================================================================================
+        Iterator i = bigrams.entrySet().iterator();
 
-	public Node bigramMLE(String first) {
+        while (i.hasNext()) {
 
-		Node res = new Node("", Double.NEGATIVE_INFINITY);
-		String s;
-		String e;
-		double min = Double.NEGATIVE_INFINITY;
-		double log;
+            s = ((Map.Entry) i.next()).getKey().toString();
 
-		Iterator i = bigrams.entrySet().iterator();
+            if (s.matches(first + " .+")) {
 
-		while (i.hasNext()) {
+                /*
+		 * I used a rough attempt to implement smoothing. The example in the textbook
+		* said that we need to add one to the numerator, and V to the denominator.
+                 */
+                
+                // .size() isn't an adequate solution, we would need V in this case.
+                e = s.split(" ")[1];
+                log = Math.log10(((double) bigrams.get(s) + 1) / (unigrams.get(e) + misc.get("UNI_V")));
 
-			s = ((Map.Entry) i.next()).getKey().toString();
+                if (log > min) {
 
-			if (s.matches(first + " .+")) {
+                    min = log;
+                    res.setValues(e, min);
 
-				/*
-				 * I used a rough attempt to implement smoothing. The example in the textbook
-				 * said that we need to add one to the numerator, and V to the denominator.
-				 */
+                }
 
-				// .size() isn't an adequate solution, we would need V in this case.
+            }
 
-				e = s.split(" ")[1];
-				log = Math.log10(((double) bigrams.get(s) + 1) / (unigrams.get(e) + unigrams.size()));
+        }
 
-				if (log > min) {
+        return res;
 
-					min = log;
-					res.setValues(e, min);
+    }
 
-				}
+    // ===========================================================================================================
+    // Calculate trigram MLE from a single word input. This is called after the
+    // input from Voce is split
+    // into fragments we can use.
+    // ===========================================================================================================
+    public Node trigramMLE(String first, String second) {
 
-			}
+        Node res = new Node("", Double.NEGATIVE_INFINITY);
+        String s;
+        String e;
+        double min = Double.NEGATIVE_INFINITY;
+        double log;
 
-		}
+        Iterator i = bigrams.entrySet().iterator();
 
-		return res;
+        while (i.hasNext()) {
 
-	}
+            s = ((Map.Entry) i.next()).getKey().toString();
 
-	// ===========================================================================================================
-	// Calculate trigram MLE from a single word input. This is called after the input from Voce is split
-	// into fragments we can use.
-	// ===========================================================================================================
+            if (s.matches(first + " " + second + " .+")) {
 
-	public Node trigramMLE(String first, String second) {
+                e = s.split(" ")[2];
+                log = Math.log10((double) trigrams.get(s) / bigrams.get(first + " " + second));
 
-		Node res = new Node("", Double.NEGATIVE_INFINITY);
-		String s;
-		String e;
-		double min = Double.NEGATIVE_INFINITY;
-		double log;
+                if (log > min) {
 
-		Iterator i = bigrams.entrySet().iterator();
+                    min = log;
+                    res.setValues(e, min);
 
-		while (i.hasNext()) {
+                }
 
-			s = ((Map.Entry) i.next()).getKey().toString();
+            }
 
-			if (s.matches(first + " " + second + " .+")) {
+        }
 
-				// .size() isn't an adequate solution, we would need V in this case.
+        return res;
 
-				e = s.split(" ")[2];
-				log = Math.log10((double) trigrams.get(s) / bigrams.get(first + " " + second));
-
-				if (log > min) {
-
-					min = log;
-					res.setValues(e, min);
-
-				}
-
-			}
-
-		}
-
-		return res;
-
-	}
+    }
 
 }
