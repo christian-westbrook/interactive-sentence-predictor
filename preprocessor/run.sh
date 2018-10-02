5e3aa0d381f7d11cdeb46d73a4140c80eb2b636d
Christian Westbrook <christianwestbrook@live.com>
2018-10-01 20:35:49 -0500

    Testing a shell script that will run the preprocessor
    This update adds compiling the Java source code generated by PS2.jj, and the removal of these class files at the end of the execution.

--- preprocessor/run.sh
+++ preprocessor/run.sh
@@ -20,15 +20,33 @@ fi
 
 mkdir data
 
+# Move into the tokenizer directory and compile the Java source code generated 
+# by the JavaCC tokenizer
+cd ./tokenizer
+javac PS2Tokenizer.java
+
 # After the script completes execution, remove each of the files generated when compiling the
-# tokenizer. 
+# tokenizer.
 
 cd ./tokenizer
 
 rm ParseException.java
+rm ParseException.class
+
 rm PS2TokenizerConstants.java
+rm PS2TokenizerConstants.class
+
 rm PS2TokenizerTokenManager.java
+rm PS2TokenizerTokenManager.class
+
 rm TokenMgrError.java
+rm TokenMgrError.class
+
 rm PS2Tokenizer.java
+rm PS2Tokenizer.class
+
 rm SimpleCharStream.java
+rm SimpleCharStream.class
+
 rm Token.java
+rm Token.class
