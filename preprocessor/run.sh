f9bcc86e2af1971599e3053b4ba5f5755d917da0
Christian Westbrook <christianwestbrook@live.com>
2018-10-01 19:48:30 -0500

    Testing a shell script that will run the preprocessor
    I started by compiling the JavaCC tokenizer in the last push. In this push I'm testing checking if a data directory exists in the root script directory. If the data directory does exist, I'm deleting it. After that control structure I'm creating a new data directory. This is where I'll direct the output of the tokenizer.

--- preprocessor/run.sh
+++ preprocessor/run.sh
@@ -7,3 +7,15 @@ set -e
 # Move into the tokenizer directory and compile the JavaCC tokenizer
 cd ./tokenizer
 javacc PS2.jj
+
+# Move back into the root directory of this script and check if the data
+# folder exists. If the data folder exists, remove it. Either way, create
+# a new data file.
+cd ..
+
+if [ -d data ]
+then
+	rm -r data
+fi
+
+mkdir data
