7d2052f2b7a35afce8981547c720cdf51a9be4a1
Christian Westbrook <christianwestbrook@live.com>
2018-10-01 20:54:40 -0500

    Testing a shell script that will run the preprocessor
    Testing corrections to the command that runs PS2Tokenizer

--- preprocessor/run.sh
+++ preprocessor/run.sh
@@ -36,7 +36,7 @@ javac PS2Tokenizer.java
 # Move back into the root directory and execute PS2Tokenizer.java. The input
 # directory is ./training-data and the output directory is ./data.
 cd ..
-java ./tokenizer/PS2Tokenizer ./training-data ./data
+java /tokenizer/PS2Tokenizer ./training-data ./data
 
 # After the script completes execution, remove each of the files generated when compiling the
 # tokenizer.
