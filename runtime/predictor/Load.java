fe488eb33a1f7e3603ad0845f811e07e92e19701
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2018-10-04 05:39:56 -0500

    Add files via upload

--- runtime/predictor/Load.java
+++ runtime/predictor/Load.java
@@ -1,3 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author fishe
+ */
 //=================================================================================================
 // Program		: Language Model Predictor
 // Class		: Load.java
@@ -5,7 +15,6 @@
 // Abstract		: This program loads a HashMap of unigrams, bigrams, and trigrams for use by the
 //				  predictor. Miscellaneous metrics are also loaded.
 //=================================================================================================
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,125 +22,110 @@ import java.io.FileReader;
 import java.io.ObjectInputStream;
 import java.util.HashMap;
 
-public class Load
-{
-	private HashMap<String, Integer> unigrams;
-	private HashMap<String, Integer> bigrams;
-	private HashMap<String, Integer> trigrams;
-	
-	private int n;
-	private int v;
-	
-	private int unigramsN;
-	private int unigramsV;
-	
-	private int bigramsN;
-	private int bigramsV;
-	
-	private int trigramsN;
-	private int trigramsV;
-	
-	public Load()
-	{
-		try
-		{
-			// Load unigram table
-			FileInputStream ufis = new FileInputStream("./data/unigrams.map");
-			ObjectInputStream uois = new ObjectInputStream(ufis);
-			unigrams = (HashMap<String, Integer>) uois.readObject();
-			uois.close();
-		
-			// Load bigram table
-			FileInputStream bfis = new FileInputStream("./data/bigrams.map");
-			ObjectInputStream bois = new ObjectInputStream(bfis);
-			bigrams = (HashMap<String, Integer>) bois.readObject();
-			bois.close();
-		
-			// Load trigram table
-			FileInputStream tfis = new FileInputStream("./data/trigrams.map");
-			ObjectInputStream tois = new ObjectInputStream(tfis);
-			trigrams = (HashMap<String, Integer>) tois.readObject();
-			tois.close();
-		
-			// Load metrics
-			File mFile = new File("./data/metrics.dat");
-			FileReader mfr = new FileReader(mFile);
-			BufferedReader bfr = new BufferedReader(mfr);
-		
-			String[] metrics = bfr.readLine().split(",");
-		
-			n = Integer.parseInt(metrics[0]);
-			v = Integer.parseInt(metrics[1]);
-		
-			unigramsN = Integer.parseInt(metrics[2]);
-			unigramsV = Integer.parseInt(metrics[3]);
-		
-			bigramsN = Integer.parseInt(metrics[4]);
-			bigramsV = Integer.parseInt(metrics[5]);
-		
-			trigramsN = Integer.parseInt(metrics[6]);
-			trigramsV = Integer.parseInt(metrics[7]);
-		}
-		catch (Exception ex)
-		{
-			ex.printStackTrace();
-			System.exit(1);
-		}
-	}
-	
-	// Getters
-	public HashMap getUnigrams()
-	{
-		return unigrams;
-	}
-	
-	public HashMap getBigrams()
-	{
-		return bigrams;
-	}
-	
-	public HashMap getTrigrams()
-	{
-		return trigrams;
-	}
-	
-	public int getN()
-	{
-		return n;
-	}
-	
-	public int getV()
-	{
-		return v;
-	}
-	
-	public int getUnigramsN()
-	{
-		return unigramsN;
-	}
-	
-	public int getUnigramsV()
-	{
-		return unigramsV;
-	}
-	
-	public int getBigramsN()
-	{
-		return bigramsN;
-	}
-	
-	public int getBigramsV()
-	{
-		return bigramsV;
-	}
-	
-	public int getTrigramsN()
-	{
-		return trigramsN;
-	}
-	
-	public int getTrigramsV()
-	{
-		return trigramsV;
-	}
+public class Load {
+
+    private HashMap<String, Integer> unigrams;
+    private HashMap<String, Integer> bigrams;
+    private HashMap<String, Integer> trigrams;
+
+    private int n;
+    private int v;
+
+    private int unigramsN;
+    private int unigramsV;
+
+    private int bigramsN;
+    private int bigramsV;
+
+    private int trigramsN;
+    private int trigramsV;
+
+    public Load() {
+        try {
+            // Load unigram table
+            FileInputStream ufis = new FileInputStream("./data/unigrams.map");
+            ObjectInputStream uois = new ObjectInputStream(ufis);
+            unigrams = (HashMap<String, Integer>) uois.readObject();
+            uois.close();
+
+            // Load bigram table
+            FileInputStream bfis = new FileInputStream("./data/bigrams.map");
+            ObjectInputStream bois = new ObjectInputStream(bfis);
+            bigrams = (HashMap<String, Integer>) bois.readObject();
+            bois.close();
+
+            // Load trigram table
+            FileInputStream tfis = new FileInputStream("./data/trigrams.map");
+            ObjectInputStream tois = new ObjectInputStream(tfis);
+            trigrams = (HashMap<String, Integer>) tois.readObject();
+            tois.close();
+
+            // Load metrics
+            File mFile = new File("./data/metrics.dat");
+            FileReader mfr = new FileReader(mFile);
+            BufferedReader bfr = new BufferedReader(mfr);
+
+            String[] metrics = bfr.readLine().split(",");
+
+            n = Integer.parseInt(metrics[0]);
+            v = Integer.parseInt(metrics[1]);
+
+            unigramsN = Integer.parseInt(metrics[2]);
+            unigramsV = Integer.parseInt(metrics[3]);
+
+            bigramsN = Integer.parseInt(metrics[4]);
+            bigramsV = Integer.parseInt(metrics[5]);
+
+            trigramsN = Integer.parseInt(metrics[6]);
+            trigramsV = Integer.parseInt(metrics[7]);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    // Getters
+    public HashMap getUnigrams() {
+        return unigrams;
+    }
+
+    public HashMap getBigrams() {
+        return bigrams;
+    }
+
+    public HashMap getTrigrams() {
+        return trigrams;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public int getV() {
+        return v;
+    }
+
+    public int getUnigramsN() {
+        return unigramsN;
+    }
+
+    public int getUnigramsV() {
+        return unigramsV;
+    }
+
+    public int getBigramsN() {
+        return bigramsN;
+    }
+
+    public int getBigramsV() {
+        return bigramsV;
+    }
+
+    public int getTrigramsN() {
+        return trigramsN;
+    }
+
+    public int getTrigramsV() {
+        return trigramsV;
+    }
 }
