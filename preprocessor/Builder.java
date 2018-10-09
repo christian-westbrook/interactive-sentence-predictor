cf04252863ba831fe12bb16c52566e2de26ca60e
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2018-10-09 10:56:30 -0500

    Update Builder.java
    Updated documentation.

--- preprocessor/Builder.java
+++ preprocessor/Builder.java
@@ -1,7 +1,7 @@
 //=================================================================================================
 // Program		: Interactive Sentence Predictor Preprocessor
 // Class		: Build.java
-// Developer	: Christian Westbrook
+// Developer		: Christian Westbrook, Renae Fisher
 // Abstract		: This program inputs token and frequency data and builds a series of ngram tables
 //                along with calculating certain metrics. All of the generated information is stored
 //                to disk to be used by the Language Model Predictor's runtime application.
